af7f515d119e71ef478ea3e95bd2d18c50ab557b
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2025-12-21 19:31:30 +0530

    feat: add tesk logic

--- src/index.js
+++ src/index.js
@@ -6,11 +6,32 @@ const data1 = {
   description: "Use an account subscription benefits on many other devices with their own account",
   date: "2025-12-21",
 };
+data1.id = crypto.randomUUID();
+data1.done = false;
+data1.tasks = [];
+
+addProjectData(data1);
+
+const task1 = {
+  name: "get info about steam family"
+};
+test1.id = crypto.randomUUID();
+test1.done = false;
+
+addTaskData(data1.id, task1);
+
 const data2 = {
   name: "Search for a hide-and-seek game",
   description: "the game should have a story with some stealth mode, could have some horror scenes for twist.",
   date: "2025-12-25",
 };
 
-addProjectData(data1);
 addProjectData(data2);
+
+const task2 = {
+  name: "launch a game on two accounts at the same time."
+};
+task2.id = crypto.randomUUID();
+task2.done = false;
+
+addTaskData(data1.id, task1);
